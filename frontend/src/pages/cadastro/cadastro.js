8fa904306b44dd798afeb75bcc5299e28e1ba917
youth_space_20 <ismaelce@hotmail.com>
2024-11-26 21:01:18 -0300

    Página Cadastro

---
+++ frontend/src/pages/cadastro/cadastro.js
@@ -0,0 +1,14 @@
+import './cadastro.css'
+
+function Cadastro() {
+    return (
+        <div>
+            <form class="form_cadastro">
+            <label>Email</label>
+            <input type="email" required />
+            </form>
+        </div>
+    )
+}
+
+export default Cadastro
