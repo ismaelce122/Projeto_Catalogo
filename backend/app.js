f96d837dc072a3e65b7fa575aa4987e6c95f3d22
pedrocauan100 <peferrarida@gmail.com>
2024-11-28 20:49:56 -0300

    Configurando Formulário de Cadastro de Produtos

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,23 @@
 import express from 'express'
-import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 import pool from './config/db.js'
 
-dotenv.config()
 const Port = 3001
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('/api/cadastrar_produtos', async (req, res) => {
+    const { nome, descricao, categoria, preco, validade } = req.body
+    const sql = 'INSERT INTO produtos (nome, descricao, categoria, preco, validade) VALUES (?, ?, ?, ?, ? )' 
+    try {
+        const [result] = await pool.query(sql, [nome, descricao, categoria, preco, validade])
+        res.send('Produto Cadastrado com Sucesso')
+    } catch(err) {
+        res.status(500).send('Erro ao Cadastrar Produto: ' + err.message)
+    }
+})
 
 app.listen(Port, () => {
     console.log('Servidor Conectado...')
--- frontend/src/components/formCadastro/formCadastro.js
+++ frontend/src/components/formCadastro/formCadastro.js
@@ -1,19 +1,53 @@
+import React, { useState } from 'react'
+import axios from 'axios'
 import './formCadastro.css'
 
 function FormCadastro() {
+    const [produtos, setProdutos] = useState({
+        nome: '',
+        descricao: '',
+        categoria: '',
+        preco: '',
+        validade: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setProdutos({...produtos, [name]: value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/api/cadastrar_produtos', produtos)
+            .then(response => {
+                console.log(response.data)
+                setProdutos({
+                    nome: '',
+                    descricao: '',
+                    categoria: '',
+                    preco: '',
+                    validade: ''
+                })
+                .catch(error => {
+                    console.error('Erro ao Cadastrar Produto: ', error)
+                })
+            })
+            console.log(produtos)
+    }
+
     return(
         <div>
-            <form className='form_cadastro'>
+            <form className='form_cadastro' onSubmit={handleSubmit} >
                 <label>Nome:</label>
-                <input className='input_produtos' type='text' name='nome' />
+                <input className='input_produtos' type='text' name='nome' onChange={handleChange} value={produtos.nome} />
                 <label>Descrição:</label>
-                <input className='input_produtos' type='text' name='descricao' />
+                <input className='input_produtos' type='text' name='descricao' onChange={handleChange} value={produtos.descricao} />
                 <label>Categoria:</label>
-                <input className='input_produtos' type='text' name='categoria' />
+                <input className='input_produtos' type='text' name='categoria' onChange={handleChange} value={produtos.categoria} />
                 <label>Preço:</label>
-                <input className='input_produtos' type='text' name='preco' />
+                <input className='input_produtos' type='text' name='preco' onChange={handleChange} value={produtos.preco} />
                 <label>Validade:</label>
-                <input className='input_produtos' type='text' name='validade' />
+                <input className='input_produtos' type='text' name='validade' onChange={handleChange} value={produtos.validade} />
                 <button className='btn_cadastrar' type='submit'>Cadastrar Produto</button>
             </form>
         </div>
