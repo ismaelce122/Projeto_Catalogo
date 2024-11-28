8e6e6d8bfc44e0e130b57b15efa000321f033d7e
Danieljeanb <163362179+Danieljeanb@users.noreply.github.com>
2024-11-28 19:08:38 -0300

    Produtos Atualizado

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ProdutoList from './pages/Produtos/ProductList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
