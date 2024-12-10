e911c2462774b232b9aae76a33a6435f4321f67f
Danieljeanb <163362179+Danieljeanb@users.noreply.github.com>
2024-12-10 20:55:42 -0300

    Carrosel Atualizado

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,10 +1,13 @@
+import ImageCarousel from '../../components/carrossel/carrossel'
 import './home.css'
 
 function Home() {
     return(
-        <div className="container text-center mt-2 zoom">
+        <div className="container text-center mt-2">
             <h1>Olá Seja Bem-Vindo</h1>
+            <ImageCarousel />
         </div>
+        
     )
 }
 
